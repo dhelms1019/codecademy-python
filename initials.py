652e1e8d159e1a8a9b3da6ab58d3bcd598dbe8c4
dhelms1019 <144491898+dhelms1019@users.noreply.github.com>
2023-12-22 13:44:34 -0600

    New initials.py file

---
+++ initials.py
@@ -0,0 +1,15 @@
+# David Helms
+# I like music.
+
+first_init_top_bottom = 'DDDD '
+first_init_sides = 'D   D'
+second_init_sides = 'H   H'
+second_init_middle = 'HHHHH'
+
+print(first_init_top_bottom, ' ', second_init_sides)
+print(first_init_sides, ' ', second_init_sides)
+print(first_init_sides, ' ', second_init_sides)
+print(first_init_sides, ' ', second_init_middle)
+print(first_init_sides, ' ', second_init_sides)
+print(first_init_sides, ' ', second_init_sides)
+print(first_init_top_bottom, ' ', second_init_sides)
